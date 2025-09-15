aeca49b228344be89e9108158f5af54128c380fd
Dr Mohamed Hossam El-Bably <Bablu521@users.noreply.github.com>
2025-09-15 15:25:44 +0300

    update git hooks

--- .github/hooks/pre-push.sh
+++ .github/hooks/pre-push.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 # ===== Pre-Push Hook =====
-# Runs Flutter checks before allowing push
+# Runs Flutter checks + branch name validation before allowing push
 
 echo "🚀 Running pre-push checks..."
+
+# 0. Validate branch name
+echo "🔍 Validating branch name..."
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+echo "Branch: $BRANCH_NAME"
+
+BRANCH_REGEX="^(bugfix|hotfix|release|feature)\/ECOM-[0-9]+\/[a-zA-Z0-9\-]+$"
+
+if echo "$BRANCH_NAME" | grep -Eq "$BRANCH_REGEX"; then
+  echo "✅ Branch name is valid."
+else
+  echo "❌ Invalid branch name!"
+  echo "✅ Allowed format: <prefix>/ECOM-<ticketNumber>-<task-name>"
+  echo "Examples:"
+  echo "   feature/ECOM-27-Initialize-Project"
+  echo "   bugfix/ECOM-102-Fix-Login"
+  exit 1
+fi
 
 # 1. Get packages
 echo "📦 Running flutter pub get..."
