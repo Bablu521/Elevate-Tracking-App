6408d17e024e7bec59c341cae01ff438ebe1b8e2
Dr Mohamed Hossam El-Bably <Bablu521@users.noreply.github.com>
2025-09-18 23:38:38 +0300

    add firebase app distrubtion to app

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,16 @@ import 'package:elevate_tracking_app/core/router/app_router.dart';
 import 'package:elevate_tracking_app/core/router/route_names.dart';
 import 'package:elevate_tracking_app/generated/l10n.dart';
 import 'package:elevate_tracking_app/my_bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   configureDependencies();
   Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
