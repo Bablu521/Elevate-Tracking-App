7ef5e9a9e44354c841948be9295f46424e0da9ee
Dr Mohamed Hossam El-Bably <Bablu521@users.noreply.github.com>
2025-09-23 15:16:22 +0300

    - Add test_app_wrapper for mock materialApp with screenUtil and theme and router in widgets tests - Add widget_keys file for saving keys of widgets used for widget test - Update app_router by separate routes list from GoRouter

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,15 +1,23 @@
 import 'package:elevate_tracking_app/core/router/route_names.dart';
-import 'package:flutter/material.dart';
+// import 'package:elevate_tracking_app/presentation/application_approved/views/screen/application_approved_screen.dart';
+// import 'package:elevate_tracking_app/presentation/onboarding/views/screen/onboarding_screen.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
+
+  static final List<GoRoute> routes = [
+    // GoRoute(
+    //   path: RouteNames.onboarding,
+    //   builder: (context, state) => const OnboardingScreen(),
+    // ),
+    // GoRoute(
+    //   path: RouteNames.applicationApproved,
+    //   builder: (context, state) => const ApplicationApprovedScreen(),
+    // ),
+  ];
+
   static final router = GoRouter(
     initialLocation: RouteNames.onboarding,
-    routes: [
-      GoRoute(
-        path: RouteNames.onboarding,
-        builder: (context, state) => const SizedBox(),
-      ),
-    ],
+    routes: routes,
   );
 }
