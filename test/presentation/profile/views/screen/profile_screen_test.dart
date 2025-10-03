2db9ee4ac48f09721f3a79f087a62d1c7482efa7
Dr Mohamed Hossam El-Bably <Bablu521@users.noreply.github.com>
2025-10-03 17:07:14 +0300

    profile screen widget test

--- test/presentation/profile/views/screen/profile_screen_test.dart
+++ test/presentation/profile/views/screen/profile_screen_test.dart
@@ -1,6 +1,8 @@
+import 'package:elevate_tracking_app/core/api_result/api_result.dart';
 import 'package:elevate_tracking_app/core/custom_widget/test_app_wrapper.dart';
 import 'package:elevate_tracking_app/core/di/di.dart';
-import 'package:elevate_tracking_app/presentation/profile/view_models/profile_view_model.dart';
+import 'package:elevate_tracking_app/domain/entites/driver_entity.dart';
+import 'package:elevate_tracking_app/domain/entites/vehicle_entity.dart';
 import 'package:elevate_tracking_app/presentation/profile/views/screen/profile_screen.dart';
 import 'package:elevate_tracking_app/domain/use_cases/edit_profile_use_case.dart';
 import 'package:elevate_tracking_app/domain/use_cases/get_logged_driver_data_use_case.dart';
@@ -10,24 +12,60 @@ import 'package:elevate_tracking_app/domain/use_cases/upload_profile_photo_use_c
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../dummy/profile_info_dummy.dart';
 import 'profile_screen_test.mocks.dart';
 
-
-@GenerateMocks([GetLoggedDriverDataUseCase, EditProfileUseCase, GetVehicleUseCase, UploadProfilePhotoUseCase, LogoutUseCase])
+@GenerateMocks([
+  GetLoggedDriverDataUseCase,
+  EditProfileUseCase,
+  GetVehicleUseCase,
+  UploadProfilePhotoUseCase,
+  LogoutUseCase,
+])
 void main() {
   group("Profile screen Widget Test", () {
-    setUpAll(() {
+    final MockGetLoggedDriverDataUseCase mockGetLoggedDriverDataUseCase =
+        MockGetLoggedDriverDataUseCase();
+    final MockEditProfileUseCase mockEditProfileUseCase =
+        MockEditProfileUseCase();
+    final MockGetVehicleUseCase mockGetVehicleUseCase = MockGetVehicleUseCase();
+    final MockUploadProfilePhotoUseCase mockUploadProfilePhotoUseCase =
+        MockUploadProfilePhotoUseCase();
+    final MockLogoutUseCase mockLogoutUseCase = MockLogoutUseCase();
+    setUpAll(() async {
       configureDependencies();
+      await getIt.unregister<GetLoggedDriverDataUseCase>();
+      getIt.registerSingleton<GetLoggedDriverDataUseCase>(
+        mockGetLoggedDriverDataUseCase,
+      );
+      await getIt.unregister<EditProfileUseCase>();
+      getIt.registerSingleton<EditProfileUseCase>(mockEditProfileUseCase);
+      await getIt.unregister<GetVehicleUseCase>();
+      getIt.registerSingleton<GetVehicleUseCase>(mockGetVehicleUseCase);
+      await getIt.unregister<UploadProfilePhotoUseCase>();
+      getIt.registerSingleton<UploadProfilePhotoUseCase>(
+        mockUploadProfilePhotoUseCase,
+      );
+      await getIt.unregister<LogoutUseCase>();
+      getIt.registerSingleton<LogoutUseCase>(mockLogoutUseCase);
     });
-     final viewModel = ProfileViewModel(
-      MockGetLoggedDriverDataUseCase(),
-      MockEditProfileUseCase(),
-      MockGetVehicleUseCase(),
-      MockUploadProfilePhotoUseCase(),
-      MockLogoutUseCase(),
-    );
 
     testWidgets('Verify Structure', (WidgetTester tester) async {
+      final expectedEntity = ProfileInfoDummy.dummyDriverEntityFake;
+      final expectedResult = ApiSuccessResult<DriverEntity>(expectedEntity);
+      provideDummy<ApiResult<DriverEntity>>(expectedResult);
+      when(
+        mockGetLoggedDriverDataUseCase.call(),
+      ).thenAnswer((_) async => expectedResult);
+      final expecteVehicledEntity = ProfileInfoDummy.dummyVehicleEntityFake;
+      final expecteVehicledResult = ApiSuccessResult<VehicleEntity>(expecteVehicledEntity);
+      provideDummy<ApiResult<VehicleEntity>>(expecteVehicledResult);
+      when(
+        mockGetVehicleUseCase.call(expectedEntity.vehicleType),
+      ).thenAnswer((_) async => expecteVehicledResult);
+
       //Arrange
       await tester.pumpWidget(TestAppWrapper(child: ProfileScreen()));
       //Assert
@@ -36,4 +74,3 @@ void main() {
     });
   });
 }
-
